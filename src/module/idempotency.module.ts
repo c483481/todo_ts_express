54173086a85e298042d54601d078f66440555721
c483481 <charleskojansow@gmail.com>
2024-03-06 18:55:24 +0800

    fix linter

--- src/module/idempotency.module.ts
+++ src/module/idempotency.module.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, RequestHandler, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 
 export interface Storage {
     has: (str: string) => boolean;
